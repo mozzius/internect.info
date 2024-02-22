f31509ba2b52db1f28a2c8a9dc7cf5d92ced322d
Samuel Newman <mozzius@protonmail.com>
2024-02-22 21:36:03 +0000

    try a different font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Bitter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 
 import { cn } from "~/lib/utils";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const inter = Bitter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "internect.info - Resolve a Bluesky handle",
