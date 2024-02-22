c84ec16bd968fa24b125a96bcbe91b86606ed4c2
Samuel Newman <mozzius@protonmail.com>
2024-02-22 21:33:33 +0000

    reorder imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { cn } from "~/lib/utils";
-
 import { Analytics } from "@vercel/analytics/react";
+
+import { cn } from "~/lib/utils";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
